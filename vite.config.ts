a3d9d0a171a778c298bf1e074b3d3d1e1789221a
Ajay <ajay.goswami@coderower.com>
2025-10-10 01:09:12 +0530

    asdas

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import inject from '@rollup/plugin-inject'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    inject({
+      $: 'jquery',
+      jQuery: 'jquery',
+    })
+  ],
   server: {
     port: 3000,
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          mui: ['@mui/material', '@mui/icons-material'],
+          utils: ['axios', 'react-hook-form', 'yup']
+        }
+      }
+    }
   },
   define: {
     global: 'globalThis',
   },
   optimizeDeps: {
-    include: ['jquery']
+    include: ['jquery', 'react-summernote-lite']
   }
 })
