e7d573fe0edc2ca89cd2c63db373c437b8aaebc2
Ajay <ajay.goswami@coderower.com>
2025-10-10 01:08:16 +0530

    feat: update Vite configuration to include jQuery in optimizeDeps and ensure globalThis definition

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,11 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true
+  },
+  define: {
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    include: ['jquery']
   }
 })
