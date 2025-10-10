b2167b7eccd1cf01774cd2b1c35c71f540b03e7b
Ajay <ajay.goswami@coderower.com>
2025-10-10 11:35:04 +0530

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import App from './App'
 import './index.css'
-import './polyfills'
 
 const theme = createTheme({
   palette: {
